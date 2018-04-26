bab2cc6ad11ecb794393c6061c1a796b5322b4dd
Dominik Lindner <d.lindner@dundee.ac.uk>
2018-04-26 15:26:14 +0100

    Overhaul of the parallel import script

--- scripts/import/parallel_image_import.sh
+++ scripts/import/parallel_image_import.sh
@@ -2,33 +2,91 @@
 
 ###
 # Use GNU parallel tool to perform the image import in parallel.
-# Runs an import job for each entry (line) in the filePaths.tsv file.
 # 
-# Usage: Adjust the filePaths setting and as appropriate the number 
-# of parallel jobs (nJobs). The other options are usually fine as 
-# they are. Check that the import options (line 28) match the options
-# specified in the bulk.yml file (unfortunately this script doesn't 
-# use the bulk.yml!)
+# Usage: Adjust the bulkFile location and as appropriate the number of 
+# parallel jobs (nJobs). The other options are usually fine as they are.  
 #
-# IMPORTANT: Log in first before launching the script!
-# ( /opt/omero/server/OMERO.server/bin/omero login )
+# Note: Check the log afterwards for errors, e.g.
+# cat log  | cut -f 7,9 | grep -v "^0"
+# Prints out the jobs which exited with an error code
 ###
 
 ### BEGIN Settings
-export omero=/opt/omero/server/OMERO.server/bin/omero
-filePaths=../../experimentA/idr0043-experimentA-filePaths.tsv
+bulkFile=../../experimentA/idr0043-experimentA-bulk.yml
+nJobs=10
 jobslogfile=log
 jobsresultdir=rslt
-nJobs=15
+export omero=/opt/omero/server/OMERO.server/bin/omero
 ### END Settings
 
+$omero login
+
+##  Preparation
+###############
+
+# Determine experiment directory
+expDir=`dirname $bulkFile`
+
+# Determine project directory and name
+projectDir=`realpath $expDir`
+projectDir=`dirname $projectDir`
+projectName=`basename $projectDir`/`basename $expDir`
+
+# Determine path to filePaths.tsv
+filePaths=$expDir/`grep "path:" $bulkFile | cut -d " " -f2 | tr -d "\""`
+
+# Create a list of dataset names
+datasets=`cat $filePaths | cut -d '	' -f1 | cut -d ':' -f3 | uniq`
+
+# Create a temporary bulk.yml with dry_run option set
+tmpBulk=$expDir/tmp.yml
+cp $bulkFile $tmpBulk
+echo 'dry_run: "true"' >> $tmpBulk
+
+# Use tmp bulk.yml to generate the list of import commands
+$omero import --bulk $tmpBulk > commands.txt
+
+rm $tmpBulk
+
+
+##  Create and link all datasets first
+######################################
+projectId=`$omero hql --ids-only --limit 1 --style plain "select p from Project p where p.name='$projectName'"`
+if [ -z $projectId ]; then
+  projectId=`$omero obj new Project name=$projectName`
+else
+  projectId=`echo $projectId | cut -d ',' -f2`
+fi
+
+for dataset in $datasets
+do
+  datasetId=`$omero hql --ids-only --limit 1 --style plain "select d from Dataset d where d.name='$dataset'"`
+  if [ -z $datasetId ]; then
+  	datasetId=`$omero obj new Dataset name=$dataset`
+  else
+  	datasetId=`echo $datasetId | cut -d ',' -f2`
+  fi
+
+  linkId=`$omero hql --ids-only --limit 1 --style plain "select l from ProjectDatasetLink l join l.child as ds where ds.name='$dataset'"`
+  if [ -z $linkId ]; then
+    $omero obj new ProjectDatasetLink parent=$projectId child=$datasetId
+  fi
+done
+
+
+##  Perform parallel image import
+#################################
+
 import_image() {
-  input=`echo $1 | expand -t1`
-  # --> Check that the import options are the same as in the bulk.yml
-  $omero import -c --transfer "ln_s" --exclude "clientpath" --checksum-algorithm "File-Size-64" --logprefix "logs/" --output "yaml" --target $input
+  options=${1//\"/}
+  $omero import $options
 }
 export -f import_image
 
+parallel -a commands.txt --jobs $nJobs --results $jobsresultdir --joblog $jobslogfile import_image
 
-parallel -a $filePaths --jobs $nJobs --results $jobsresultdir --joblog $jobslogfile import_image
+rm commands.txt
+
+$omero logout
+
 

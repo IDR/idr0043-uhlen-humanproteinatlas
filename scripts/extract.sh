50aa7cf2e6a23c65e5ce5b28d0fc3474409aa18b
Sébastien Besson <seb.besson@gmail.com>
2018-06-26 16:39:55 +0100

    Fixes

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -3,7 +3,7 @@
 # Each upload is structured as follows
 # - assays.txt       the assays file for the upload
 # - md5sum.txt       a checksums file
-# - 101.tar          the indidivual TRA files containing the imaging data
+# - 101.tar          the indidivual TAR files containing the imaging data
 # - 102.tar
 # Each TAR file will be untarred into a directory containing GZIP'ed TIFF
 # files
@@ -13,5 +13,5 @@ N=${N:-10}
 
 #
 find $PREFIX*.tar > files.txt
-parallel -a files.txt --jobs 10 --joblog log --results results "cat md5sum.txt | grep ' {}' | md5sum -c && tar xvf {} && rm {} && gunzip {/.}/*.gz"
+parallel -a files.txt --jobs $N --joblog log --results results "cat md5sum.txt | grep ' {}' | md5sum -c && tar xvf {} && rm {} && gunzip {/.}/*.gz"
 rm files.txt

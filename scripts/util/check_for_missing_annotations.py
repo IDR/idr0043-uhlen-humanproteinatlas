584125f3164c27d6623d3969bb5d84bc74b7f3a5
Dominik Lindner <d.lindner@dundee.ac.uk>
2018-12-18 15:40:18 +0000

    Reformatted to fix flake8

--- scripts/util/check_for_missing_annotations.py
+++ scripts/util/check_for_missing_annotations.py
@@ -4,27 +4,31 @@ import pandas
 import sys
 import os
 
+
 def printusage():
-  print ('''
-  This script checks for missing annotations and prints the dataset name,
-  image name and image id of images which don't have any annotations to stdout.
-  Progress information is printed to stderr.
+    print ('''
+This script checks for missing annotations and prints the dataset name,
+image name and image id of images which don't have any annotations to stdout.
+Progress information is printed to stderr.
 
-  Usage: python check_for_missing_annotations.py [path to annotation.csv] [Project ID] >> no_annotations.csv
+Usage: python check_for_missing_annotations.py [path to annotation.csv] \
+[Project ID] >> no_annotations.csv
 
-  Environment variables OMERO_USER and OMERO_PASSWORD need to be set.
-  (OMERO_HOST and OMERO_PORT are supported but optional)
-  ''')
-  sys.exit(1)
+Environment variables OMERO_USER and OMERO_PASSWORD need to be set.
+(OMERO_HOST and OMERO_PORT are supported but optional)
+          ''')
+    sys.exit(1)
+
 
 if len(sys.argv) < 3:
-  printusage()
+    printusage()
 else:
-  annoFile = sys.argv[1]
-  projectId = sys.argv[2]
+    annoFile = sys.argv[1]
+    projectId = sys.argv[2]
 
-if not (os.environ.get('OMERO_USER') and  os.environ.get('OMERO_PASSWORD')):
-  printusage()
+if not (os.environ.get('OMERO_USER') and
+        os.environ.get('OMERO_PASSWORD')):
+    printusage()
 
 host = os.environ.get('OMERO_HOST', 'localhost')
 port = int(os.environ.get('OMERO_PORT', '4064'))
@@ -34,7 +38,8 @@ df = pandas.read_csv(annoFile)
 for index, row in df.iterrows():
     datasets.add(row["Dataset Name"])
 
-conn = BlitzGateway(os.environ['OMERO_USER'], os.environ['OMERO_PASSWORD'], host=host, port=port)
+conn = BlitzGateway(os.environ['OMERO_USER'], os.environ['OMERO_PASSWORD'],
+                    host=host, port=port)
 conn.connect()
 
 print("Dataset,Image,ID")
@@ -43,8 +48,8 @@ total = len(datasets)
 count = 0
 for ds in project.listChildren():
     if ds.name in datasets:
-      print("Checking dataset %d / %d" % (count, total), file=sys.stderr)
-      count = count + 1
-      for img in ds.listChildren():
-        if len(list(img.listAnnotations())) == 0:
-          print("%s,%s,%d" % (ds.name, img.getName(), img.getId()))
+        print("Checking dataset %d / %d" % (count, total), file=sys.stderr)
+        count = count + 1
+        for img in ds.listChildren():
+            if len(list(img.listAnnotations())) == 0:
+                print("%s,%s,%d" % (ds.name, img.getName(), img.getId()))
